2a834dd4ad21c489270f3450123e3bb530508278
karan-10001 <karanpratapsingh10001@gmail.com>
2023-04-06 15:18:11 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import HomePage from "./Pages/HomePage";
 import Skills from "./Pages/Skills";
 import Projects from "./Pages/Projects";
 import { Routes, Route } from "react-router-dom";
-import Error from "./Pages/Error";
+// import Error from "./Pages/Error";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<HomePage />} />
         <Route path="/about" element={<About />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<Error />} />
+        {/* <Route path="*" element={<Error />} /> */}
       </Routes>
     </>
   );
